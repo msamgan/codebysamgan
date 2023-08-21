598429a805194d2e85f1cbdf6a985861d658dfba
msamgan <mohdsamgankhan@gmail.com>
2023-08-21 15:48:54 -0400

    removed smartlook.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -26,22 +26,6 @@
             src="https://platform-api.sharethis.com/js/sharethis.js#property=64e3a1db4265f4001221c93f&product=sticky-share-buttons&source=platform"
             async="async"></script>
 
-    <script type='text/javascript'>
-        window.smartlook || (function (d) {
-            var o = smartlook = function () {
-                o.api.push(arguments)
-            }, h = d.getElementsByTagName('head')[0];
-            var c = d.createElement('script');
-            o.api = [];
-            c.async = true;
-            c.type = 'text/javascript';
-            c.charset = 'utf-8';
-            c.src = 'https://web-sdk.smartlook.com/recorder.js';
-            h.appendChild(c);
-        })(document);
-        smartlook('init', '410e83c977d6125b1cbfe76dde34c9eae557b826', {region: 'eu'});
-    </script>
-
 </head>
 <body class="font-sans antialiased">
 @inertia
