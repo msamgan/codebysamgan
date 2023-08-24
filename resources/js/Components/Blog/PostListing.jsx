692e23a40cb397d622550f96869601469ed1fabd
Mohd Samgan Khan <mohdsamgankhan@gmail.com>
2023-08-24 19:49:39 -0400

    Unsplash (#5)
    * image on home page.
    
    * added image in post page and listing.

--- resources/js/Components/Blog/PostListing.jsx
+++ resources/js/Components/Blog/PostListing.jsx
@@ -9,12 +9,15 @@ export default function PostListing({posts}) {
                 {
                     posts.map((post) => {
                         return (
-                            <li key={post.id}>
+                            <li key={post.id} className={post.featured_image ? 'flex flex-row' : 'w-full'}>
                                 <article className={'border-b pb-6'}>
                                     <dl>
                                         <dt className="sr-only">Published on</dt>
                                         <dd className="text-xs text-gray-600">
-                                            <time dateTime="2021-01-12">{formatDate(post.publish_date)}</time>
+                                            <time
+                                                dateTime={formatDate(post.publish_date)}>
+                                                {formatDate(post.publish_date)}
+                                            </time>
                                         </dd>
                                     </dl>
                                     <div className="space-y-1">
@@ -28,6 +31,12 @@ export default function PostListing({posts}) {
                                         </div>
                                     </div>
                                 </article>
+                                {
+                                    post.featured_image
+                                        ? <img className={'w-1/5 rounded-lg ml-3'} src={post.featured_image}
+                                               alt={post.title}/>
+                                        : ''
+                                }
                             </li>
                         )
                     })
