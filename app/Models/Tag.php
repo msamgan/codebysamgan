18a24a04b231da6397d8d77823292a89cdc8f23b
Mohd Samgan Khan <mohdsamgankhan@gmail.com>
2023-12-06 21:57:08 -0500

    update for mobile (#7)

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Jenssegers\Agent\Agent;
 use Wink\WinkTag;
 
 class Tag extends WinkTag
@@ -18,7 +19,9 @@ class Tag extends WinkTag
 
     public static function topTags(): Collection|array
     {
-        return self::query()->withCount('posts')->orderBy('posts_count', 'desc')->limit(10)->get();
+        $limit = (new Agent())->isMobile() ? 5 : 10;
+
+        return self::query()->withCount('posts')->orderBy('posts_count', 'desc')->limit($limit)->get();
     }
 
     public function getRouteKeyName(): string
