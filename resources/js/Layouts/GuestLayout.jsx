a983b0b4eda3bd1d48539e61c9c70cd2a72968ef
msamgan <mohdsamgankhan@gmail.com>
2024-01-10 11:58:27 -0500

    updated banner.

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -2,6 +2,34 @@ import {Head} from '@inertiajs/react';
 import Header from "@/Components/Blog/Header.jsx";
 
 
+const FxBanner = () => {
+    return (
+        <div className="p-8 text-orange-600 bg-white" style={{
+            marginTop: 0
+        }}>
+            <div className="container mx-auto">
+                <div className="flex flex-col items-center justify-between">
+                    <img className={'w-80 h-72'} src={'https://www.frameworkx.info/img/frameworkx.png'}
+                         alt={'frameworkx'}/>
+                    <h2 className="text-center text-5xl tracki font-bold text-black">
+                        Your Accelerated Path to <span className={'text-orange-600'}>API Creation</span>
+                    </h2>
+                    <p className={'text-2xl text-black max-w-5xl'}>
+                        I created Framework X to be all about substance over style!. A well-oiled machine for
+                        API development. Please check it out. All and Any feedback is appreciated.
+                    </p>
+                    <a href="https://www.frameworkx.info/docs/introduction" rel="noreferrer noopener"
+                       target={'_blank'}
+                       className="px-5 mt-4 lg:mt-0 py-3 rounded-md border block bg-orange-600 text-gray-50 border-orang\e-600 font-bold">
+                        Get Started
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
 export default function Guest({children, head}) {
     return (<>
         <Head>
@@ -24,27 +52,7 @@ export default function Guest({children, head}) {
             <div className="flex flex-col min-h-screen space-y-12">
                 <Header/>
 
-                <div className="p-6 py-12 text-orange-600 bg-grey">
-                    <div className="container mx-auto">
-                        <div className="flex flex-col items-center justify-between">
-
-                            <img className={'w-80 h-72'} src={'https://www.frameworkx.info/img/frameworkx.png'}
-                                 alt={'frameworkx'}/>
-                            <h2 className="text-center text-5xl tracki font-bold text-black">
-                                Your Accelerated Path to <span className={'text-orange-600'}>API Creation</span>
-                            </h2>
-                            <p className={'text-2xl text-black max-w-5xl'}>
-                                I created Framework X to be all about substance over style!. A well-oiled machine for
-                                API development. Please check it out. All and Any feedback is appreciated.
-                            </p>
-                            <a href="https://www.frameworkx.info/docs/introduction" rel="noreferrer noopener"
-                               target={'_blank'}
-                               className="px-5 mt-4 lg:mt-0 py-3 rounded-md border block bg-orange-600 text-gray-50 border-orang\e-600 font-bold">
-                                Get Started
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                <FxBanner />
 
                 <main className="flex-1 max-w-5xl px-6 mx-auto space-y-12 pb-20">
                     {children}
